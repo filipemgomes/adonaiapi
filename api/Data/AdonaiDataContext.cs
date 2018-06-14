60c5408a3b3349b7dae3fed188c871b2ed22d2f5
Filipe Gomes <=>
2018-06-13 22:59:25 -0300

    created appsettings and DataContext for this app

--- api/Data/AdonaiDataContext.cs
+++ api/Data/AdonaiDataContext.cs
@@ -1,7 +1,15 @@
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace api.Data
 {
-    public class AdonaiDataContext
+    public class AdonaiDataContext : DbContext
     {
-        
+        public AdonaiDataContext()
+        {
+
+        }
+
+        public DbSet<Lead> Lead { get; set; }
     }
 }
